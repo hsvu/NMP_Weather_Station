9cd4bb6d47a3642b854bfab13047188805380346
Bleacheng <tudmangamers@gmail.com>
2021-03-17 15:49:59 +1100

    Just to test that github is properly working on my laptop. Also added a comment

--- emulator/backend.ts
+++ emulator/backend.ts
@@ -1,7 +1,7 @@
 /*
 
     Right now, this is basically the same as the weather_output_test.ts file. Need to add
-    the data validation part. 
+    the data validation part. There are also other ways to do this I think.
 
 */
 import {generate_weather_data} from "./weather_output";
