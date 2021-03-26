440d274a8d7f945d6830cabbe6c5d9e6982074cd
Bleacheng <tudmangamers@gmail.com>
2021-03-26 22:51:06 +1100

    Prints to console and sends UDP message to localhost at port 12000

--- emulator/src/weather_output_test.ts
+++ emulator/src/weather_output_test.ts
@@ -2,9 +2,17 @@ import {generate_weather_data} from "./weather_output";
 import dgram from "dgram";
 
 async function print_weather_data() {
+    const HOST = "localhost";
+    const PORT = 12000;
     const data = generate_weather_data();
     console.log(data);
     var buf = Buffer.from(JSON.stringify(data), 'utf8');
+    var client = dgram.createSocket('udp4');
+    client.send(buf, 0, buf.length, PORT, HOST, function(err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        client.close();
+    });
 }
 
 setInterval(print_weather_data, 1000);
