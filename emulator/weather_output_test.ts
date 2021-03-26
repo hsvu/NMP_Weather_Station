81b23e2782bd8d4857c29e297ba82fcecbd6de3d
Bleacheng <tudmangamers@gmail.com>
2021-03-26 20:52:05 +1100

    Part 2

--- emulator/weather_output_test.ts
+++ emulator/weather_output_test.ts
@@ -1,7 +1,10 @@
 import {generate_weather_data} from "./weather_output";
+import "dgram";
 
-async function print_weather_data() {
-    console.log(generate_weather_data());    
+function print_weather_data() {
+    const data = generate_weather_data();
+    console.log(data);
+    var buf = Buffer.from(JSON.stringify(data), 'utf8');    
 }
 
 setInterval(print_weather_data, 1000);
